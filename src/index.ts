13aefc140ce264c5b85ae153c55afa820e92ad35
moki <morozov.kirill.moki@gmail.com>
2023-04-11 21:03:28 +0400

    fix(infra): package build

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-export * from 'src/renderer';
-export * from 'src/plugin';
+import {
+    MarkdownRendererMode,
+    MarkdownRendererParams,
+    MarkdownRendererEnv,
+    MarkdownRendererState,
+    MarkdownRenderer,
+} from 'src/renderer';
+
+import {mdRenderer} from 'src/plugin';
+
+export {
+    MarkdownRendererMode,
+    MarkdownRendererParams,
+    MarkdownRendererEnv,
+    MarkdownRendererState,
+    MarkdownRenderer,
+    mdRenderer,
+};
+
+export * as default from './index';
