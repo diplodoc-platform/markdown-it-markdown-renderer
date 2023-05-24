bfbab9cdfc0add28240d1f9352d34e739a8cb7bf
moki <morozov.kirill.moki@gmail.com>
2023-05-24 13:55:53 +0400

    refactor(rules/diplodoc): notes

--- src/rules/diplodoc/notes.ts
+++ src/rules/diplodoc/notes.ts
@@ -2,6 +2,7 @@ import Renderer from 'markdown-it/lib/renderer';
 import Token from 'markdown-it/lib/token';
 
 import {MarkdownRenderer} from 'src/renderer';
+import {isList} from 'src/rules/block/list';
 
 const notes: Renderer.RenderRuleRecord = {
     yfm_note_open: function (this: MarkdownRenderer, tokens: Token[], i: number) {
@@ -49,15 +50,8 @@ const notes: Renderer.RenderRuleRecord = {
 
         rendered += this.EOL;
 
-        const isList = (container: any) =>
-            container?.type === 'ordered_list_open' || container?.type === 'bullet_list_open';
-
-        const epsilon = this.containers.reduce(
-            (acc, container) => (isList(container) ? acc + 2 : 0),
-            1,
-        );
-
-        if (i + epsilon !== tokens.length) {
+        const closeTokens = this.containers.reduce((n, token) => (isList(token) ? n + 2 : 0), 0);
+        if (i + closeTokens + 1 !== tokens.length) {
             rendered += this.EOL;
         }
 
