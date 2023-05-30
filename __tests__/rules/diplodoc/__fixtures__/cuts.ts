2724259be47d82dbb3655d9c08fda3dc591fa59b
moki <morozov.kirill.moki@gmail.com>
2023-05-30 19:02:00 +0400

    test(rules/diplodoc): update cuts fixture

--- __tests__/rules/diplodoc/__fixtures__/cuts.ts
+++ __tests__/rules/diplodoc/__fixtures__/cuts.ts
@@ -39,6 +39,74 @@ content after cut
         section: 'cuts',
         number: 3,
     },
+    {
+        markdown: `\
+- item one
+
+  {% cut "cut title" %}
+
+  content inside cut
+
+  {% endcut %}
+
+  content
+`,
+        expectedMarkdown: `\
+- item one
+
+  {% cut "cut title" %}
+
+  content inside cut
+
+  {% endcut %}
+  
+  content
+`,
+        html: '',
+        section: 'cuts',
+        number: 4,
+    },
+    {
+        markdown: `\
+-
+  {% cut "cut title" %}
+
+  content inside cut
+
+  {% endcut %}
+`,
+        html: '',
+        section: 'cuts',
+        number: 5,
+    },
+    {
+        markdown: `\
+1. one
+   1. one two
+
+      {% cut "cut title" %}
+
+      content inside cut
+
+      {% endcut %}
+2. two
+`,
+        html: '',
+        section: 'cuts',
+        number: 6,
+    },
+    {
+        markdown: `\
+- {% cut "list cut" %}
+
+  content
+
+  {% endcut %}
+`,
+        html: '',
+        section: 'cuts',
+        number: 7,
+    },
 ];
 
 export {cuts};
