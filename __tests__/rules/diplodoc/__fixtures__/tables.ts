5e718c8fe11372a95888efc13dad18906bd349ad
moki <morozov.kirill.moki@gmail.com>
2023-06-06 17:50:03 +0400

    test(diplodoc): gfm table inside containers fixtures

--- __tests__/rules/diplodoc/__fixtures__/tables.ts
+++ __tests__/rules/diplodoc/__fixtures__/tables.ts
@@ -24,6 +24,63 @@ after gfm table
         section: 'tables',
         number: 2,
     },
+    {
+        markdown: `\
+> |Header 1|Header 2|
+> |-----------|-----------|
+> |[link text](https://www.google.com)|**bold text**|
+> |![image text](image.png "hint")|*italics*|
+`,
+        html: '',
+        section: 'tables',
+        number: 3,
+    },
+    {
+        markdown: `\
+- |Header 1|Header 2|
+  |-----------|-----------|
+  |[link text](https://www.google.com)|**bold text**|
+  |![image text](image.png "hint")|*italics*|
+- |Header 1|Header 2|
+  |-----------|-----------|
+  |[link text](https://www.google.com)|**bold text**|
+  |![image text](image.png "hint")|*italics*|
+
+  haha
+- |Header 1|Header 2|
+  |-----------|-----------|
+  |[link text](https://www.google.com)|**bold text**|
+  |![image text](image.png "hint")|*italics*|
+- |Header 1|Header 2|
+  |-----------|-----------|
+  |[link text](https://www.google.com)|**bold text**|
+  |![image text](image.png "hint")|*italics*|
+`,
+        html: '',
+        section: 'tables',
+        number: 4,
+    },
+    {
+        markdown: `\
+1. |Header 1|Header 2|
+   |-----------|-----------|
+   |[link text](https://www.google.com)|**bold text**|
+   |![image text](image.png "hint")|*italics*|
+
+   text
+2. |Header 1|Header 2|
+   |-----------|-----------|
+   |[link text](https://www.google.com)|**bold text**|
+   |![image text](image.png "hint")|*italics*|
+3. |Header 1|Header 2|
+   |-----------|-----------|
+   |[link text](https://www.google.com)|**bold text**|
+   |![image text](image.png "hint")|*italics*|
+`,
+        html: '',
+        section: 'tables',
+        number: 5,
+    },
 ];
 
 export {tables};
