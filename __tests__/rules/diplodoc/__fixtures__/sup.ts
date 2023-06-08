88803e7d7e6b86c6e5997cccd0b25f36e862e2c8
moki <morozov.kirill.moki@gmail.com>
2023-06-08 15:35:40 +0400

    test(rules/diplodoc): sup

---
+++ __tests__/rules/diplodoc/__fixtures__/sup.ts
@@ -0,0 +1,21 @@
+const sup = [
+    {
+        markdown: `\
+paragrah with 29^th^ sup text
+`,
+        html: '',
+        section: 'sup',
+        number: 1,
+    },
+    {
+        markdown: `\
+this is [1^st^ link](file.md) this is [2^nd^ link](file-2.md)
+`,
+        html: '',
+        section: 'sup',
+        number: 2,
+    },
+];
+
+export {sup};
+export default {sup};
