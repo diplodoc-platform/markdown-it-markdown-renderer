d3daab8ee44b7abb34335794ffff59beeb4b8311
moki <morozov.kirill.moki@gmail.com>
2023-06-08 15:17:40 +0400

    refactor(rules/inline): simple markup inline rules

---
+++ src/rules/inline/basic.ts
@@ -0,0 +1,23 @@
+import Token from 'markdown-it/lib/token';
+
+const rules = new Set([
+    'em_open',
+    'em_close',
+    'strong_open',
+    'strong_close',
+    'sup_open',
+    'sup_close',
+]);
+
+function basic(tokens: Token[], i: number) {
+    const {type, markup} = tokens[i];
+
+    if (rules.has(type)) {
+        return markup;
+    }
+
+    throw new Error('failed to render emphasis');
+}
+
+export {basic};
+export default {basic};
