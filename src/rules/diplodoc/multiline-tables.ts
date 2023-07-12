3f9b430822d041c0684a6fc16bbc44b23fe7437f
moki <morozov.kirill.moki@gmail.com>
2023-07-12 20:20:31 +0400

    feat(rules/diplodoc): multiline tables

---
+++ src/rules/diplodoc/multiline-tables.ts
@@ -0,0 +1,87 @@
+import Renderer from 'markdown-it/lib/renderer';
+import Token from 'markdown-it/lib/token';
+import {MarkdownRenderer} from 'src/renderer';
+
+const always =
+    <T>(a: T) =>
+    () =>
+        a;
+
+const alwaysEmptyStr = always('');
+
+const interrupters = new Set(['yfm_td_close', 'bullet_list_open', 'ordered_list_open']);
+
+const multilineTables: Renderer.RenderRuleRecord = {
+    yfm_tbody_open: alwaysEmptyStr,
+    yfm_tbody_close: alwaysEmptyStr,
+    yfm_table_open: function (this: MarkdownRenderer, tokens: Token[], i: number) {
+        let rendered = '';
+
+        if (i && tokens[i - 1].type !== 'yfm_td_open') {
+            rendered += this.EOL.repeat(2);
+        }
+
+        rendered += this.renderContainer(tokens[i]);
+
+        rendered += '#|';
+
+        return rendered;
+    },
+    yfm_table_close: function (this: MarkdownRenderer, tokens: Token[], i: number) {
+        let rendered = '';
+
+        rendered += this.renderContainer(tokens[i]);
+
+        rendered += '|#';
+
+        return rendered;
+    },
+    yfm_tr_open: function (this: MarkdownRenderer, tokens: Token[], i: number) {
+        let rendered = '';
+
+        if (tokens[i - 1].type !== 'yfm_tr_close') {
+            rendered += '\n';
+        }
+
+        rendered += this.renderContainer(tokens[i]);
+
+        rendered += '|';
+
+        return rendered;
+    },
+    yfm_tr_close: function () {
+        return '|\n';
+    },
+    yfm_td_open: function (this: MarkdownRenderer, tokens: Token[], i: number) {
+        let rendered = '|';
+
+        const next = tokens[i + 1];
+        if (!next) {
+            return rendered;
+        }
+
+        if (!interrupters.has(next.type)) {
+            rendered += '\n';
+        }
+
+        return rendered;
+    },
+    yfm_td_close: function (this: MarkdownRenderer, tokens: Token[], i: number) {
+        let rendered = '';
+
+        if (tokens[i - 1].type !== 'yfm_td_open') {
+            rendered += '\n';
+        }
+
+        rendered += this.renderContainer(tokens[i]);
+
+        if (i + 1 !== tokens.length && tokens[i + 1].type !== 'yfm_td_open') {
+            rendered += '|';
+        }
+
+        return rendered;
+    },
+};
+
+export {multilineTables};
+export default {multilineTables};
