9bf3ea20fa1a9db9ee76157ba75daeced5196c16
moki <morozov.kirill.moki@gmail.com>
2023-10-31 22:00:16 +0400

    fix(handlers/diplodoc): anchors

--- src/handlers/diplodoc/anchors.ts
+++ src/handlers/diplodoc/anchors.ts
@@ -17,45 +17,80 @@ const anchors = {
             );
         }
 
-        const next = tokens[i + 1];
-        if (next.type !== 'inline') {
+        const inline = tokens[i + 1];
+        if (inline?.type !== 'inline') {
             return '';
         }
 
-        if (!next.children) {
-            next.children = [];
-        }
-
-        const anchorLinkStartIdx = next.children.findIndex(({type}) => type === 'link_open');
-        const anchorLinkEndtIdx = next.children.findIndex(({type}) => type === 'link_close');
-        if (anchorLinkEndtIdx !== -1 && anchorLinkStartIdx !== -1) {
-            next.children.splice(anchorLinkStartIdx, anchorLinkEndtIdx - anchorLinkStartIdx + 1);
-        }
-
-        const [row] = tokens[i].map ?? [];
-        // eslint-disable-next-line eqeqeq, no-eq-null
-        if (row == null) {
-            throw new Error('failed to render anchor, no line mapping on header');
-        }
-
-        const line = normalizeSource(source)[row];
-        if (!line?.length) {
-            throw new Error('failed to render anchor, incorrect source or line mapping');
-        }
-
-        const anchor = normalizeSource(source)[row].match(/\{#([^{]+)\}/gimu);
-        if (!anchor?.length) {
+        const {children} = inline;
+        if (!children?.length) {
             return '';
         }
 
-        const anchorToken = new Token('text', '', 0);
-        anchorToken.content = ` ${anchor[anchor.length - 1]}`;
+        const {numberOfAnchors} = foldAnchorLinksIntoAnchors(tokens, i, children);
+        if (!numberOfAnchors) {
+            return '';
+        }
 
-        next.children.push(anchorToken);
+        if (isImplicitAnchor(tokens, i, source)) {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            tokens[i + 1].children!.splice(0, 1);
+
+            return '';
+        }
+
+        const anchorTokens = children.slice(0, numberOfAnchors);
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        tokens[i + 1].children!.splice(0, numberOfAnchors);
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        splicePushBack(tokens[i + 1].children!, ...anchorTokens.reverse());
 
         return '';
     },
 };
+
+function foldAnchorLinksIntoAnchors(tokens: Token[], i: number, children: Token[]) {
+    let numberOfAnchors = 0;
+    for (let j = 0; j < children.length; j++) {
+        if (children[j].type === 'anchor_hidden_desc') {
+            numberOfAnchors++;
+
+            const linkOpen = children[j - 1];
+            const href = linkOpen.attrGet('href');
+            if (!href?.length) {
+                throw new Error('failed to render anchor');
+            }
+
+            const anchorToken = new Token('anchor', '', 0);
+            anchorToken.content = href;
+
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            tokens[i + 1].children!.splice(j - 1, 3, anchorToken);
+        }
+    }
+
+    return {numberOfAnchors};
+}
+
+function isImplicitAnchor(tokens: Token[], i: number, source: string[]) {
+    const [row] = tokens[i].map ?? [];
+    // eslint-disable-next-line eqeqeq, no-eq-null, @typescript-eslint/no-non-null-assertion
+    if (row == null) {
+        throw new Error('failed to render anchor, no line mapping on header');
+    }
+
+    const line = normalizeSource(source)[row];
+    if (!line?.length) {
+        throw new Error('failed to render anchor, incorrect source or line mapping');
+    }
+
+    return !line.match(/\{#([^{]+)\}/gmu);
+}
+
+function splicePushBack<T>(xs: T[], ...values: T[]) {
+    xs.splice(xs.length, 0, ...values);
+}
 
 export {anchors};
 export default {anchors};
