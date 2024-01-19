34d4a783f71e47ffbeabaa0d3b3d7285bb04525b
moki <morozov.kirill.moki@gmail.com>
2024-01-19 13:18:29 +0400

    fix(renderer/hooks/tabs): handle nested tabs

--- src/hooks/diplodoc/reorderTabs.ts
+++ src/hooks/diplodoc/reorderTabs.ts
@@ -2,82 +2,67 @@ import Token from 'markdown-it/lib/token';
 import {CustomRendererHookParameters} from '@diplodoc/markdown-it-custom-renderer';
 
 function reorderTabs(parameters: CustomRendererHookParameters) {
-    let start;
-    let end;
-    let shift = 0;
-
-    const contents: Array<Array<Token>> = [];
-    const indexes: Array<number> = [];
-    const shifts: Array<number> = [];
-
-    for (let i = 0; i < parameters.tokens.length; i++) {
-        const {type} = parameters.tokens[i];
-        if (type === 'tab_close') {
-            indexes.push(i);
-            continue;
-        }
-
-        if (type === 'tab-panel_open') {
-            start = i;
-            continue;
-        }
-
-        if (type === 'tab-panel_close') {
-            end = i;
-        }
-
-        // eslint-disable-next-line no-eq-null, eqeqeq
-        if (start == null || end == null) {
-            continue;
-        }
-
-        const content: Array<Token> = [];
-        for (let j = start; j < end + 1; j++) {
-            content.push(parameters.tokens[j]);
-
-            // eslint-disable-next-line no-param-reassign
-            parameters.tokens[j] = new Token('markdown-renderer-trash', '', 0);
-            // eslint-disable-next-line no-param-reassign
-            parameters.tokens[j].content = '';
-        }
-
-        start = null;
-        end = null;
-
-        contents.push(content);
-        shift += content.length;
-        shifts.push(shift);
-    }
-
-    for (let i = 0; i < contents.length; i++) {
-        const index = indexes[i] + (shifts[i - 1] ?? 0);
-
-        parameters.tokens.splice(index, 0, ...contents[i]);
-    }
-
-    for (let i = 0; i < parameters.tokens.length; i++) {
-        const token = parameters.tokens[i];
-        if (token.type === 'tab-list_close') {
-            start = i + 1;
-            continue;
-        }
-
-        if (token.type === 'tabs_close') {
-            end = i;
-        }
-
-        // eslint-disable-next-line no-eq-null, eqeqeq
-        if (start == null || end == null) {
-            continue;
-        }
-
-        parameters.tokens.splice(start, end - start);
-
-        i = i - (end - start);
-        start = end = null;
-    }
+    const bounds = findTabsContentBounds(parameters.tokens);
+    moveTabContentIntoTab(parameters.tokens, bounds);
 
     return '';
+}
+
+function findTabsContentBounds(tokens: Token[]): Array<[number, number]> {
+    const startIndecies: number[] = [];
+    const endIndecies: number[] = [];
+
+    for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+        if (token.type === 'tab-panel_open') {
+            startIndecies.push(i);
+        }
+    }
+
+    for (let i = startIndecies.length - 1; i >= 0; i--) {
+        const start = startIndecies[i];
+
+        for (let j = start; j < tokens.length; j++) {
+            const token = tokens[j];
+
+            if (token.type === 'tab-panel_close' && !endIndecies.includes(j)) {
+                endIndecies.unshift(j);
+                break;
+            }
+        }
+    }
+
+    return startIndecies.map((start: number, i: number) => [start, endIndecies[i]]);
+}
+
+function moveTabContentIntoTab(tokens: Token[], bounds: Array<[number, number]>) {
+    if (!tokens.some(isOutOfOrder)) {
+        return;
+    }
+
+    const [left, right] = bounds.pop() ?? [];
+    if (!(left && right)) {
+        throw new Error('failed to receive tab content bounds');
+    }
+    const id = tokens[left].attrGet('id');
+
+    const tabIndex = tokens.findIndex((token: Token) => {
+        const ariaControls = token.attrGet('aria-controls');
+        return ariaControls === id;
+    });
+
+    const len = right - left + 1;
+    const content = tokens.splice(left, len);
+    tokens.splice(tabIndex + 2, 0, ...content);
+
+    // eslint-disable-next-line no-param-reassign
+    bounds = bounds.map(([l, r]) => (tabIndex < l ? [l + len, r + len] : [l, r]));
+
+    moveTabContentIntoTab(tokens, bounds);
+}
+
+function isOutOfOrder(token: Token, i: number, tokens: Token[]) {
+    return token.type === 'tab_open' && tokens[i + 2]?.type !== 'tab-panel_open';
 }
 
 export {reorderTabs};
